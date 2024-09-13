a931ed07cd999e8c0743f10a6a72a170891e91d7
FlowerHeap <flowerheap@qq.com>
2024-09-14 00:24:33 +0800

    feat: 🎸 firing

--- js-test/index.js
+++ js-test/index.js
@@ -1,9 +1,17 @@
-<!--
+/*
  * @Author: FlowerHeap flowerheap@qq.com
- * @Date: 2024-09-13 23:52:14
+ * @Date: 2024-09-13 23:55:15
  * @LastEditors: FlowerHeap flowerheap@qq.com
- * @LastEditTime: 2024-09-13 23:58:35
- * @FilePath: \undefinedc:\Users\Administrator\Desktop\Reborn\README.md
+ * @LastEditTime: 2024-09-14 00:07:51
+ * @FilePath: \Reborn\js-test\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
-# 重走来时路
+ */
+//变量提升
+
+function student() {
+  a = 1;
+  console.log('a', a);
+}
+student();
+console.log('a', a);
+//todo: 111
